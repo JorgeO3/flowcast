15f5891fb3ba7e5f2f88b8e66e0d2a9fd684375f
Jorge Osorio <joheosmo@gmail.com>
2024-09-16 13:01:02 -0500

    fix: fix the hydratation error from radix-ui

--- web/app.vue
+++ web/app.vue
@@ -1,5 +1,14 @@
+<script lang="ts" setup>
+import { ConfigProvider } from "radix-vue";
+import { useId } from "vue";
+
+const useIdFunction = () => useId() || "";
+</script>
+
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <ConfigProvider :use-id="useIdFunction">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </ConfigProvider>
 </template>
