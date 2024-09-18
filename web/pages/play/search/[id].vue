4416adfd7c6ccdcdbfc218cb17c10ffb9efc35bf
Jorge Osorio <joheosmo@gmail.com>
2024-09-18 10:22:59 -0500

    feat(play): add play pages

---
+++ web/pages/play/search/[id].vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+definePageMeta({ layout: "play" });
+
+const { params } = useRoute();
+</script>
+
+<template>
+  <div>
+    <h1>{{ params.id }}</h1>
+  </div>
+</template>
---
+++ web/pages/play/search/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+definePageMeta({ layout: "play" });
+</script>
+
+<template>
+  <div>
+    <h1>Search</h1>
+  </div>
+</template>
+
+<style></style>
