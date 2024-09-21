cda467c2f2ae7617a21866e86465bd212d09b207
Jorge Osorio <joheosmo@gmail.com>
2024-09-21 00:32:37 -0500

    chore: update the radix dependency. Finally solve the rendering problems

--- web/app.vue
+++ web/app.vue
@@ -1,15 +1,5 @@
-<script lang="ts" setup>
-import { ConfigProvider } from "radix-vue";
-import { useId } from "vue";
-
-const useIdFunction = () => useId() || "";
-</script>
-
 <template>
-  <!-- @vue-ignore -->
-  <ConfigProvider :use-id="useIdFunction">
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </ConfigProvider>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
