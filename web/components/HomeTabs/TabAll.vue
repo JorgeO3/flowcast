2160bac6fa926d7ff32fed7736f1a4af60e759f3
Jorge Osorio <joheosmo@gmail.com>
2024-09-21 11:27:42 -0500

    style: rename the folder

---
+++ web/components/HomeTabs/TabAll.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { sections } from "@/utils/tabAllSections.js";
+</script>
+
+<template>
+  <div class="flex flex-col h-full">
+    <div class="flex-1 overflow-y-auto px-4">
+      <div class="flex flex-col w-full">
+        <template v-for="section in sections" :key="section.link">
+          <MusicSection v-bind="section" />
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
