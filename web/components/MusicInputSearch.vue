16acd45c16cc95f857f5f55e4d24259fb11dc3ab
Jorge Osorio <joheosmo@gmail.com>
2024-09-21 11:17:38 -0500

    chore: move music HeadInputSearch component to global level

--- web/components/MusicInputSearch.vue
+++ web/components/MusicInputSearch.vue
@@ -1,18 +1,20 @@
 <script lang="ts" setup>
 import { Search } from "lucide-vue-next";
+
+const router = useRouter();
+const search = useState("searchValue", () => "");
 </script>
 
 <template>
-  <form class="ml-auto flex-1 sm:flex-initial">
+  <form class="flex-1 sm:flex-initial">
     <div class="relative">
       <Search class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
       <Input
         type="search"
-        placeholder="Search products..."
+        v-model="search"
+        placeholder="What do you want to play?"
         class="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
       />
     </div>
   </form>
 </template>
-
-<style></style>
