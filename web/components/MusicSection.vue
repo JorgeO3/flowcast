919ca00ad481cdb783cfe8a9ab9d7fca6c316d0f
Jorge Osorio <joheosmo@gmail.com>
2024-09-21 11:34:02 -0500

    feat(play): add section component

---
+++ web/components/MusicSection.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { Separator } from "@/components/ui/separator";
+import type { Section, MediaContent } from "~/utils/tabAllSections.js";
+import SectionHeader from "./MusicSection/SectionHeader.vue";
+
+interface Props extends Section {}
+
+defineProps<Props>();
+
+const sliceBoxes = (boxes: MediaContent[]) => {
+  return boxes.slice(0, 5);
+};
+</script>
+
+<template>
+  <div class="flex flex-col w-full h-fit mb-16">
+    <!-- Header -->
+    <SectionHeader :title :text :link />
+
+    <Separator class="my-4" />
+
+    <div class="flex justify-around h-72">
+      <MusicMediaBox
+        v-for="box in sliceBoxes(boxes)"
+        :key="box.title"
+        :="box"
+      />
+    </div>
+  </div>
+</template>
---
+++ web/components/MusicSection/SectionHeader.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+defineProps<{ title: string; text: string; link: string }>();
+</script>
+
+<template>
+  <div class="flex items-center justify-between">
+    <div class="space-y-1">
+      <h2 class="text-2xl font-semibold tracking-tight">{{ title }}</h2>
+      <p class="text-sm text-muted-foreground">
+        {{ text }}
+      </p>
+    </div>
+
+    <NuxtLink
+      :to="link"
+      class="text-sm font-medium hover:text-foreground text-muted-foreground"
+    >
+      All
+    </NuxtLink>
+  </div>
+</template>
